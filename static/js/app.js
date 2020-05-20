cc78d4bcc5fbb5ab4d040c169fb15c7a129bd7d1
Lewserv <lewgirl4@yahoo.com>
2020-05-19 20:12:27 -0400

    add data table

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,37 @@
 // from data.js
 var tableData = data;
 
-console.log(tableData);
+//console.log(tableData);
+
+// get table reference
+tbody = d3.select("tbody");
+
+//create a function that builds the table
+function buildTable(data) {
+    console.log(data);
+    //clear out the table
+    tbody.html("");
+
+    //loop through each object/sighting
+    data.forEach((tablerow) =>{
+         //append table row
+         var row = tbody.append('tr');
+         //append table data
+         Object.values(tablerow).forEach((value)=>{
+         var cell = row.append('td');
+            cell.text(value);
+        }
+        );
+    } )
+
+   };
+
+//create a function to handle click
+
+
+//attach an event listener to the button we created
+
+
+//build the table when the page loads
+buildTable(tableData);
 
